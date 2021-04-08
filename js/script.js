ad026eb44c3a090761533d0672bd5191fed59792
amorales2402 <82065106+amorales2402@users.noreply.github.com>
2021-04-08 12:20:33 -0600

    UJpdate
    Update of menu sections, HTML and CSS

--- js/script.js
+++ js/script.js
@@ -1,10 +1,8 @@
-
-
-(function ($) {
-    $(window).scroll(function () {
+(function($) {
+    $(window).scroll(function() {
         var scroll = $(window).scrollTop();
 
-        if (scroll >= 100) {
+        if (scroll >= 400) {
             $(".navbar").addClass("logo_b");
         } else {
             $(".navbar").removeClass("logo_b");
